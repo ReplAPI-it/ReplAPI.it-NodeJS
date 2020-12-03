a55b80fbc8d35f5b967e8bbec1cc8351f71e81f2
RayhanADev <72509475+RayhanADev@users.noreply.github.com>
2020-12-03 13:00:05 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const User = require('./src/User.js');
+const user = require('./src/User.js');
+const post = require('./src/Post.js');
+const comment = require('./src/Comment.js');
+const leaderboard = require('./src/Leaderboard.js');
 
 module.exports = {
-  User: User.User
+  User: user.User,
+  Post: post.Post,
+  Comment: comment.Comment,
+  Leaderboard: leaderboard.Leaderboard
 }
