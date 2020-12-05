f8b329e1e9ff0d6a8c187d6b2b21ce10de47e85e
RayhanADev <72509475+RayhanADev@users.noreply.github.com>
2020-12-05 17:09:07 -0600

    Create Login.js

---
+++ src/classes/Login.js
@@ -0,0 +1,52 @@
+let headers = require('../utils/headers.js');
+let variables = require('../utils/variables.js');
+
+async function _getCookies(user, pass) {
+	if (user == 'RayhanADev') {
+		let info = await variables
+			.fetch('https://repl.it/login', {
+				method: 'POST',
+				headers,
+				body: JSON.stringify({
+					user,
+					pass
+				})
+			})
+			.then(res => {
+				return res.headers.raw()['set-cookie'][1];
+			});
+
+		if (!info) {
+			throw new Error(`Couldn't fetch cookie data.`);
+		} else {
+			return info;
+		}
+	} else {
+		throw new Error(
+			`${user} is not whitelisted. Please contact @RayhanADev in Repl.it to talk about getting added to the whitelist.`
+		);
+	}
+}
+
+class Login {
+	constructor(username, password) {
+		this.username = username;
+		this.password = password;
+	}
+
+	async account() {
+		let user = this.username;
+		let pass = this.password;
+		if (user == 'RayhanADev') {
+			global.cookies = await _getCookies(user, pass);
+		} else {
+			throw new Error(
+				`${user} is not whitelisted. Please contact @RayhanADev in Repl.it to talk about getting added to the whitelist.`
+			);
+		}
+	}
+}
+
+module.exports = {
+	Login: Login
+};
