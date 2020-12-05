9a2b995bc175fa16b2360368e68fd85a46f69557
RayhanADev <72509475+RayhanADev@users.noreply.github.com>
2020-12-05 17:09:31 -0600

    Create Misc.js

---
+++ src/classes/Misc.js
@@ -0,0 +1,46 @@
+let headers = require('../utils/headers.js');
+let variables = require('../utils/variables.js');
+
+class Misc {
+	async userSearch(query, limit) {
+		if (!global.cookies) {
+			throw new Error('Not logged in.');
+		} else {
+			if (!query)
+				throw new Error(
+					'User Search needs a query to search. Please supply a query.'
+				);
+			if (!limit) limit = 10;
+
+			headers.Cookie = global.cookies;
+			let info = await variables
+				.fetch('https://repl.it/graphql', {
+					method: 'POST',
+					headers,
+					body: JSON.stringify({
+						query: `
+				  query UserSearch($query: String!, $limit: Int!) {
+          	usernameSearch(query: $query, limit: $limit) {
+              ${variables.userAttributes}
+            }
+          }`,
+						variables: {
+							query,
+							limit
+						}
+					})
+				})
+				.then(res => res.json());
+
+			if (!info.data.usernameSearch) {
+				throw new Error(`Cannot fetch users.`);
+			} else {
+				return info.data.usernameSearch;
+			}
+		}
+	}
+}
+
+module.exports = {
+	Misc: Misc
+};
