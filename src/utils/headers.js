0777816687a7eff590e4879494f6b2150727aa05
RayhanADev <72509475+RayhanADev@users.noreply.github.com>
2020-12-05 17:07:06 -0600

    Create headers.js

---
+++ src/utils/headers.js
@@ -0,0 +1,9 @@
+module.exports = {
+	'Content-Type': 'application/json',
+	Accept: 'application/json',
+	'Accept-Encoding': 'gzip, deflate, br',
+	Connection: 'keep-alive',
+	'X-Requested-With': 'REPLAPIit',
+	Referrer: 'https://repl.it',
+	Origin: 'https://repl.it'
+};
