e4c5c55d7eb98c43ebf0fac94162bd5fdd56aa58
RayhanADev <72509475+RayhanADev@users.noreply.github.com>
2020-12-10 10:56:35 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ let replapi = require('./src')
 module.exports = {
 	User: replapi.User,
 	Post: replapi.Post,
+	Repl: replapi.Repl,
 	Comment: replapi.Comment,
 	Leaderboard: replapi.Leaderboard,
+	Board: replapi.Board,
 	Notifications: replapi.Notifications,
 	Misc: replapi.Misc,
 	Login: replapi.Login
