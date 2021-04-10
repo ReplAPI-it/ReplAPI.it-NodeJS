d3b76692a27cca71841a0af45db6065c2171e886
Rayhan Arayilakath <rayhanadev@pm.me>
2021-04-10 19:28:25 +0000

    Add Basic Initialization Function

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 let replapi = require('./src')
 
-module.exports = {
-	User: replapi.User,
-	Post: replapi.Post,
-	Repl: replapi.Repl,
-	Comment: replapi.Comment,
-	Leaderboard: replapi.Leaderboard,
-  Languages: replapi.Languages,
-	Board: replapi.Board,
-	Notifications: replapi.Notifications,
-	Misc: replapi.Misc,
-	Login: replapi.Login,
-	CustomDataQuery: replapi.CustomDataQuery,
-	CustomRecursiveQuery: replapi.CustomRecursiveQuery
+module.exports = function(initVariables) {
+  global.initVariables = initVariables
+  return {
+    	User: replapi.User,
+    	Post: replapi.Post,
+    	Repl: replapi.Repl,
+    	Comment: replapi.Comment,
+    	Leaderboard: replapi.Leaderboard,
+      Languages: replapi.Languages,
+    	Board: replapi.Board,
+    	Notifications: replapi.Notifications,
+    	Misc: replapi.Misc,
+    	Login: replapi.Login,
+    	CustomDataQuery: replapi.CustomDataQuery,
+    	CustomRecursiveQuery: replapi.CustomRecursiveQuery
+  }
 };
