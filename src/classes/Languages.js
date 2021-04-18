31f153a43905aade025134bfcdcd6c9cc2affc7d
Ray Arayilakath <rayhanadev@pm.me>
2021-04-18 21:25:13 +0000

    Refactored Language Class (and lint)

--- src/classes/Languages.js
+++ src/classes/Languages.js
@@ -1,28 +1,28 @@
-import HTMLParser from 'node-html-parser'
-import fetch from 'node-fetch'
-import atob from 'atob'
+import HTMLParser from 'node-html-parser';
+import fetch from 'node-fetch';
+import atob from 'atob';
 
-async function _fetchVariable() {
-  let html = await fetch('https://staging.replit.com/', {
+async function fetchVariable() {
+  const html = await fetch('https://staging.replit.com/', {
     method: 'GET',
     headers: {
       'X-Requested-With': 'ReplAPI.it',
-      'Referrer': 'https://staging.replit.com/'
-    }
-  }).then(res => res.text());
-  
-  let root = HTMLParser.parse(html, {
+      Referrer: 'https://staging.replit.com/',
+    },
+  }).then((res) => res.text());
+
+  const root = HTMLParser.parse(html, {
     lowerCaseTagName: false,
     comment: false,
     blockTextElements: {
       script: true,
       noscript: true,
       style: true,
-      pre: true
-    }
+      pre: true,
+    },
   });
-  
-  return JSON.parse(atob(root.childNodes[1].childNodes[0].childNodes[1].childNodes[0].rawText.split("'")[1].split("'")[0]))
+
+  return JSON.parse(atob(root.childNodes[1].childNodes[0].childNodes[1].childNodes[0].rawText.split("'")[1].split("'")[0]));
 }
 
 export default class Languages {
@@ -31,12 +31,12 @@ export default class Languages {
   }
 
   async getLang() {
-    let langs = await _fetchVariable();
+    const langs = await fetchVariable();
     return langs[this.lang];
   }
 
   async getAllLangs() {
-    let langs = await _fetchVariable();
+    const langs = await fetchVariable();
     return langs;
   }
 }
