f09327b27cd723a13b3d13d7ec11e55e28261727
Ray Arayilakath <rayhanadev@pm.me>
2021-04-18 21:22:03 +0000

    Refactor Custom Class (and lint)

--- src/classes/Custom.js
+++ src/classes/Custom.js
@@ -1,146 +1,142 @@
-import fetch from 'node-fetch'
+import fetch from 'node-fetch';
 
-import headers from '../utils/headers.js'
-import constants from '../utils/constants.js'
+import headers from '../utils/headers.js';
+import constants from '../utils/constants.js';
 
 export class CustomDataQuery {
-	constructor(queryName, customQuery, customVariables) {
-		this.queryName = queryName;
-		this.customQuery = customQuery;
-		this.customVariables = customVariables;
-	}
+  constructor(queryName, customQuery, customVariables) {
+    this.queryName = queryName;
+    this.customQuery = customQuery;
+    this.customVariables = customVariables;
+  }
 
-	async getData() {
-		let queryName = this.queryName;
-		let customQuery = this.customQuery;
-		let customVariables = this.customVariables;
-		
-		let specialQueryVariables = {
-		  since: 'KarmaSince',
-		  count: 'Int',
-		  id: 'Int',
-		  limit: 'Int'
-		}
-		Object.freeze(specialQueryVariables);
-		
-		let queryVariables = Object.keys(customVariables);
-		let queryVariablesString = '';
-		for (let i = 0; i < queryVariables.length; i++) {
-		  let type;
-      if(specialQueryVariables.hasOwnProperty(queryVariables[i])) {
-	      type = String(specialQueryVariables[queryVariables[i]]);
+  async getData() {
+    const { queryName } = this;
+    const { customQuery } = this;
+    const { customVariables } = this;
+
+    const specialQueryVariables = {
+      since: 'KarmaSince',
+      count: 'Int',
+      id: 'Int',
+      limit: 'Int',
+    };
+    Object.freeze(specialQueryVariables);
+
+    const queryVariables = Object.keys(customVariables);
+    let queryVariablesString = '';
+    for (let i = 0; i < queryVariables.length; i += 1) {
+      let type;
+      if (Object.prototype.hasOwnProperty.call(specialQueryVariables, queryVariables[i])) {
+        type = String(specialQueryVariables[queryVariables[i]]);
       } else {
         type = String(typeof queryVariables[i]).split('');
         type[0] = type[0].toUpperCase();
-	      type = type.join('')
+        type = type.join('');
       }
 
-		  if(i !== queryVariables.length - 1) {
-		    queryVariablesString += `$${queryVariables[i]}: ${type}!, `
-		  } else {
-		    queryVariablesString += `$${queryVariables[i]}: ${type}!`
-		  }
-		}
+      if (i !== queryVariables.length - 1) {
+        queryVariablesString += `$${queryVariables[i]}: ${type}!, `;
+      } else {
+        queryVariablesString += `$${queryVariables[i]}: ${type}!`;
+      }
+    }
 
-		let info = await fetch(variables.graphql, {
-				method: 'POST',
-				headers,
-				body: JSON.stringify({
-					query: `
-    			  query ${queryName}(${queryVariablesString}) {
-    				  ${customQuery}
-    				}`,
-					variables: JSON.stringify(customVariables)
-				})
-			})
-			.then(res => res.json());
-    
-		if (info.errors) {
-			throw new Error(`Custom Data Query ${this.queryName} returned an error: ${JSON.stringify(info.errors)}`);
-		} else {
-			return info.data;
-		}
-	}
+    const info = await fetch(constants.graphql, {
+      method: 'POST',
+      headers,
+      body: JSON.stringify({
+        query: `
+          query ${queryName}(${queryVariablesString}) {
+            ${customQuery}
+          }`,
+        variables: JSON.stringify(customVariables),
+      }),
+    }).then((res) => res.json());
+
+    if (info.errors) {
+      throw new Error(`Custom Data Query ${this.queryName} returned an error: ${JSON.stringify(info.errors)}`);
+    } else {
+      return info.data;
+    }
+  }
 }
 
 export class CustomRecursiveQuery {
-	constructor(queryName, customQuery, customVariables, treePath, customAfter, customCount) {
-		this.queryName = queryName;
-		this.customQuery = customQuery;
-		this.customVariables = customVariables;
-		this.treePath = treePath;
-		this.customAfter = customAfter;
-		this.customCount = customCount;
-	}
+  constructor(queryName, customQuery, customVariables, treePath, customAfter, customCount) {
+    this.queryName = queryName;
+    this.customQuery = customQuery;
+    this.customVariables = customVariables;
+    this.treePath = treePath;
+    this.customAfter = customAfter;
+    this.customCount = customCount;
+  }
 
-	async getData() {
-		let queryName = this.queryName;
-		let customQuery = this.customQuery;
-		let customVariables = this.customVariables;
-		let treePath = this.treePath;
-		let customAfter = this.customAfter;
-		let customCount = this.customCount;
-		
-		let specialQueryVariables = {
-		  since: 'KarmaSince',
-		  count: 'Int',
-		  id: 'Int',
-		  limit: 'Int'
-		}
-		Object.freeze(specialQueryVariables);
-		
-		let queryVariables = Object.keys(customVariables);
-		let queryVariablesString = '';
-		for (let i = 0; i < queryVariables.length; i++) {
-		  let type;
-      if(specialQueryVariables.hasOwnProperty(queryVariables[i])) {
-	      type = String(specialQueryVariables[queryVariables[i]]);
+  async getData() {
+    const {
+      queryName, customQuery, customVariables, treePath, customAfter, customCount,
+    } = this;
+
+    const specialQueryVariables = {
+      since: 'KarmaSince',
+      count: 'Int',
+      id: 'Int',
+      limit: 'Int',
+    };
+    Object.freeze(specialQueryVariables);
+
+    const queryVariables = Object.keys(customVariables);
+    let queryVariablesString = '';
+    for (let i = 0; i < queryVariables.length; i += 1) {
+      let type;
+      if (Object.prototype.hasOwnProperty.call(specialQueryVariables, queryVariables[i])) {
+        type = String(specialQueryVariables[queryVariables[i]]);
       } else {
         type = String(typeof queryVariables[i]).split('');
         type[0] = type[0].toUpperCase();
-	      type = type.join('')
+        type = type.join('');
       }
 
-		  if(i !== queryVariables.length - 1) {
-		    queryVariablesString += `$${queryVariables[i]}: ${type}!, `
-		  } else {
-		    queryVariablesString += `$${queryVariables[i]}: ${type}!`
-		  }
-		}
-    
-		let output = [];
+      if (i !== queryVariables.length - 1) {
+        queryVariablesString += `$${queryVariables[i]}: ${type}!, `;
+      } else {
+        queryVariablesString += `$${queryVariables[i]}: ${type}!`;
+      }
+    }
 
-	 	async function recurse(after) {
-			if (after === null) return;
+    const output = [];
 
-  		let info = await variables
-  			.fetch(variables.graphql, {
-  				method: 'POST',
-  				headers,
-  				body: JSON.stringify({
-  					query: `
-      			  query ${queryName}(${queryVariablesString}) {
-      				  ${customQuery}
-      				}`,
-  					variables: JSON.stringify(customVariables)
-  				})
-  			})
-  			.then(res => res.json());
-  		
-			if (info.errors) {
-				throw new Error(`Custom Recusive Query ${queryName} returned an error: ${JSON.stringify(info.errors)}`);
-			} else {
-				info.data[customQuery.trim().split('(')[0]][treePath].items.forEach(item => {
-					output.push(item);
-				});
-				if (output.length !== customCount) {
-					await recurse(info.data[customQuery.trim().split('(')[0]][treePath].pageInfo.nextCursor);
-				}
-			}
+    async function recurse(recurseAfter) {
+      if (recurseAfter === null) return;
 
-		}
+      const info = await fetch(constants.graphql, {
+        method: 'POST',
+        headers,
+        body: JSON.stringify({
+          query: `
+            query ${queryName}(${queryVariablesString}) {
+              ${customQuery}
+            }`,
+          variables: JSON.stringify({
+            ...customVariables,
+            after: recurseAfter,
+          }),
+        }),
+      }).then((res) => res.json());
 
-		await recurse(this.customAfter);
-		return output;
-	}
+      if (info.errors) {
+        throw new Error(`Custom Recusive Query ${queryName} returned an error: ${JSON.stringify(info.errors)}`);
+      } else {
+        info.data[customQuery.trim().split('(')[0]][treePath].items.forEach((item) => {
+          output.push(item);
+        });
+        if (output.length !== customCount) {
+          await recurse(info.data[customQuery.trim().split('(')[0]][treePath].pageInfo.nextCursor);
+        }
+      }
+    }
+
+    await recurse(customAfter);
+    return output;
+  }
 }
