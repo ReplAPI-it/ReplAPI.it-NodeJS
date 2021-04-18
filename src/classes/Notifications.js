5030df132ce751cd42f9a9ae7ac3782a666a76c7
Ray Arayilakath <rayhanadev@pm.me>
2021-04-18 22:13:47 +0000

    Change Notification Class Structure to Accommodate Experimental Features

--- src/classes/Notifications.js
+++ src/classes/Notifications.js
@@ -3,333 +3,343 @@ import fetch from 'node-fetch';
 import headers from '../utils/headers.js';
 import constants from '../utils/constants.js';
 
-export default class Notifications {
-  async postReplyNotification(after, count) {
-    if (!global.cookies) {
-      throw new Error('Not logged in.');
-    } else {
-      headers.Cookie = global.cookies;
-      const info = await fetch(constants.graphql, {
-        method: 'POST',
-        headers,
-        body: JSON.stringify({
-          query: `
-            query RepliedToPostNotification($after: String!, $count: Int!) {
-              notifications(after: $after, count: $count) {
-                items {
-                  ... on RepliedToPostNotification {
-                    id
-                    url
-                    text
-                    seen
-                    context
-                    creator {
-                      ${constants.userAttributes}
+let exportable;
+
+if (global.initVariables.experimental) {
+  exportable = class Notifications {
+    async postReplyNotification(after, count) {
+      if (!global.cookies) {
+        throw new Error('Not logged in.');
+      } else {
+        headers.Cookie = global.cookies;
+        const info = await fetch(constants.graphql, {
+          method: 'POST',
+          headers,
+          body: JSON.stringify({
+            query: `
+              query RepliedToPostNotification($after: String!, $count: Int!) {
+                notifications(after: $after, count: $count) {
+                  items {
+                    ... on RepliedToPostNotification {
+                      id
+                      url
+                      text
+                      seen
+                      context
+                      creator {
+                        ${constants.userAttributes}
+                      }
+                      timeCreated
+                      timeUpdated
                     }
-                    timeCreated
-                    timeUpdated
+                  }
+                  pageInfo {
+                    nextCursor
                   }
                 }
-                pageInfo {
-                  nextCursor
-                }
-              }
-            }`,
-          variables: JSON.stringify({
-            after,
-            count,
+              }`,
+            variables: JSON.stringify({
+              after,
+              count,
+            }),
           }),
-        }),
-      })
-        .then((res) => res.json());
+        })
+          .then((res) => res.json());
 
-      if (!info.data.notifications) {
-        throw new Error('Cannot fetch notifications.');
-      } else {
-        return info.data.notifications;
+        if (!info.data.notifications) {
+          throw new Error('Cannot fetch notifications.');
+        } else {
+          return info.data.notifications;
+        }
       }
     }
-  }
 
-  async commentReplyNotification(after, count) {
-    if (!global.cookies) {
-      throw new Error('Not logged in.');
-    } else {
-      headers.Cookie = global.cookies;
-      const info = await fetch(constants.graphql, {
-        method: 'POST',
-        headers,
-        body: JSON.stringify({
-          query: `
-            query RepliedToCommentNotification($after: String!, $count: Int!) {
-              notifications(after: $after, count: $count) {
-                items {
-                  ... on RepliedToCommentNotification {
-                    id
-                    url
-                    text
-                    seen
-                    context
-                    creator {
-                      ${constants.userAttributes}
+    async commentReplyNotification(after, count) {
+      if (!global.cookies) {
+        throw new Error('Not logged in.');
+      } else {
+        headers.Cookie = global.cookies;
+        const info = await fetch(constants.graphql, {
+          method: 'POST',
+          headers,
+          body: JSON.stringify({
+            query: `
+              query RepliedToCommentNotification($after: String!, $count: Int!) {
+                notifications(after: $after, count: $count) {
+                  items {
+                    ... on RepliedToCommentNotification {
+                      id
+                      url
+                      text
+                      seen
+                      context
+                      creator {
+                        ${constants.userAttributes}
+                      }
+                      timeCreated
+                      timeUpdated
                     }
-                    timeCreated
-                    timeUpdated
+                  }
+                  pageInfo {
+                    nextCursor
                   }
                 }
-                pageInfo {
-                  nextCursor
-                }
-              }
-            }`,
-          variables: JSON.stringify({
-            after,
-            count,
+              }`,
+            variables: JSON.stringify({
+              after,
+              count,
+            }),
           }),
-        }),
-      })
-        .then((res) => res.json());
+        })
+          .then((res) => res.json());
 
-      if (!info.data.notifications) {
-        throw new Error('Cannot fetch notifications.');
-      } else {
-        return info.data.notifications;
+        if (!info.data.notifications) {
+          throw new Error('Cannot fetch notifications.');
+        } else {
+          return info.data.notifications;
+        }
       }
     }
-  }
 
-  async postMentionedNotification(after, count) {
-    if (!global.cookies) {
-      throw new Error('Not logged in.');
-    } else {
-      headers.Cookie = global.cookies;
-      const info = await fetch(constants.graphql, {
-        method: 'POST',
-        headers,
-        body: JSON.stringify({
-          query: `
-            query MentionedInPostNotification($after: String!, $count: Int!) {
-              notifications(after: $after, count: $count) {
-                items {
-                  ... on MentionedInPostNotification {
-                    id
-                    url
-                    text
-                    seen
-                    context
-                    creator {
-                      ${constants.userAttributes}
+    async postMentionedNotification(after, count) {
+      if (!global.cookies) {
+        throw new Error('Not logged in.');
+      } else {
+        headers.Cookie = global.cookies;
+        const info = await fetch(constants.graphql, {
+          method: 'POST',
+          headers,
+          body: JSON.stringify({
+            query: `
+              query MentionedInPostNotification($after: String!, $count: Int!) {
+                notifications(after: $after, count: $count) {
+                  items {
+                    ... on MentionedInPostNotification {
+                      id
+                      url
+                      text
+                      seen
+                      context
+                      creator {
+                        ${constants.userAttributes}
+                      }
+                      timeCreated
+                      timeUpdated
                     }
-                    timeCreated
-                    timeUpdated
+                  }
+                  pageInfo {
+                    nextCursor
                   }
                 }
-                pageInfo {
-                  nextCursor
-                }
-              }
-            }`,
-          variables: JSON.stringify({
-            after,
-            count,
+              }`,
+            variables: JSON.stringify({
+              after,
+              count,
+            }),
           }),
-        }),
-      })
-        .then((res) => res.json());
+        })
+          .then((res) => res.json());
 
-      if (!info.data.notifications) {
-        throw new Error('Cannot fetch notifications.');
-      } else {
-        return info.data.notifications;
+        if (!info.data.notifications) {
+          throw new Error('Cannot fetch notifications.');
+        } else {
+          return info.data.notifications;
+        }
       }
     }
-  }
 
-  async commentMentionedNotification(after, count) {
-    if (!global.cookies) {
-      throw new Error('Not logged in.');
-    } else {
-      headers.Cookie = global.cookies;
-      const info = await fetch(constants.graphql, {
-        method: 'POST',
-        headers,
-        body: JSON.stringify({
-          query: `
-            query MentionedInCommentNotification($after: String!, $count: Int!) {
-              notifications(after: $after, count: $count) {
-                items {
-                  ... on MentionedInPostNotification {
-                    id
-                    url
-                    text
-                    seen
-                    context
-                    creator {
-                      ${constants.userAttributes}
+    async commentMentionedNotification(after, count) {
+      if (!global.cookies) {
+        throw new Error('Not logged in.');
+      } else {
+        headers.Cookie = global.cookies;
+        const info = await fetch(constants.graphql, {
+          method: 'POST',
+          headers,
+          body: JSON.stringify({
+            query: `
+              query MentionedInCommentNotification($after: String!, $count: Int!) {
+                notifications(after: $after, count: $count) {
+                  items {
+                    ... on MentionedInPostNotification {
+                      id
+                      url
+                      text
+                      seen
+                      context
+                      creator {
+                        ${constants.userAttributes}
+                      }
+                      timeCreated
+                      timeUpdated
                     }
-                    timeCreated
-                    timeUpdated
+                  }
+                  pageInfo {
+                    nextCursor
                   }
                 }
-                pageInfo {
-                  nextCursor
-                }
-              }
-            }`,
-          variables: JSON.stringify({
-            after,
-            count,
+              }`,
+            variables: JSON.stringify({
+              after,
+              count,
+            }),
           }),
-        }),
-      })
-        .then((res) => res.json());
+        })
+          .then((res) => res.json());
 
-      if (!info.data.notifications) {
-        throw new Error('Cannot fetch notifications.');
-      } else {
-        return info.data.notifications;
+        if (!info.data.notifications) {
+          throw new Error('Cannot fetch notifications.');
+        } else {
+          return info.data.notifications;
+        }
       }
     }
-  }
 
-  async answerNotification(after, count) {
-    if (!global.cookies) {
-      throw new Error('Not logged in.');
-    } else {
-      headers.Cookie = global.cookies;
-      const info = await fetch(constants.graphql, {
-        method: 'POST',
-        headers,
-        body: JSON.stringify({
-          query: `
-            query AnswerAcceptedNotification($after: String!, $count: Int!) {
-              notifications(after: $after, count: $count) {
-                items {
-                  ... on AnswerAcceptedNotification {
-                    id
-                    url
-                    text
-                    seen
-                    context
-                    creator {
-                      ${constants.userAttributes}
+    async answerNotification(after, count) {
+      if (!global.cookies) {
+        throw new Error('Not logged in.');
+      } else {
+        headers.Cookie = global.cookies;
+        const info = await fetch(constants.graphql, {
+          method: 'POST',
+          headers,
+          body: JSON.stringify({
+            query: `
+              query AnswerAcceptedNotification($after: String!, $count: Int!) {
+                notifications(after: $after, count: $count) {
+                  items {
+                    ... on AnswerAcceptedNotification {
+                      id
+                      url
+                      text
+                      seen
+                      context
+                      creator {
+                        ${constants.userAttributes}
+                      }
+                      timeCreated
+                      timeUpdated
                     }
-                    timeCreated
-                    timeUpdated
+                  }
+                  pageInfo {
+                    nextCursor
                   }
                 }
-                pageInfo {
-                  nextCursor
-                }
-              }
-            }`,
-          variables: JSON.stringify({
-            after,
-            count,
+              }`,
+            variables: JSON.stringify({
+              after,
+              count,
+            }),
           }),
-        }),
-      })
-        .then((res) => res.json());
+        })
+          .then((res) => res.json());
 
-      if (!info.data.notifications) {
-        throw new Error('Cannot fetch notifications.');
-      } else {
-        return info.data.notifications;
+        if (!info.data.notifications) {
+          throw new Error('Cannot fetch notifications.');
+        } else {
+          return info.data.notifications;
+        }
       }
     }
-  }
 
-  async multiplayerInviteNotification(after, count) {
-    if (!global.cookies) {
-      throw new Error('Not logged in.');
-    } else {
-      headers.Cookie = global.cookies;
-      const info = await fetch(constants.graphql, {
-        method: 'POST',
-        headers,
-        body: JSON.stringify({
-          query: `
-            query MultiplayerInvitedNotification($after: String!, $count: Int!) {
-              notifications(after: $after, count: $count) {
-                items {
-                  ... on MultiplayerInvitedNotification {
-                    id
-                    url
-                    text
-                    seen
-                    context
-                    creator {
-                      ${constants.userAttributes}
+    async multiplayerInviteNotification(after, count) {
+      if (!global.cookies) {
+        throw new Error('Not logged in.');
+      } else {
+        headers.Cookie = global.cookies;
+        const info = await fetch(constants.graphql, {
+          method: 'POST',
+          headers,
+          body: JSON.stringify({
+            query: `
+              query MultiplayerInvitedNotification($after: String!, $count: Int!) {
+                notifications(after: $after, count: $count) {
+                  items {
+                    ... on MultiplayerInvitedNotification {
+                      id
+                      url
+                      text
+                      seen
+                      context
+                      creator {
+                        ${constants.userAttributes}
+                      }
+                      timeCreated
+                      timeUpdated
                     }
-                    timeCreated
-                    timeUpdated
+                  }
+                  pageInfo {
+                    nextCursor
                   }
                 }
-                pageInfo {
-                  nextCursor
-                }
-              }
-            }`,
-          variables: JSON.stringify({
-            after,
-            count,
+              }`,
+            variables: JSON.stringify({
+              after,
+              count,
+            }),
           }),
-        }),
-      })
-        .then((res) => res.json());
+        })
+          .then((res) => res.json());
 
-      if (!info.data.notifications) {
-        throw new Error('Cannot fetch notifications.');
-      } else {
-        return info.data.notifications;
+        if (!info.data.notifications) {
+          throw new Error('Cannot fetch notifications.');
+        } else {
+          return info.data.notifications;
+        }
       }
     }
-  }
 
-  async allNotification(after, count) {
-    if (!global.cookies) {
-      throw new Error('Not logged in.');
-    } else {
-      headers.Cookie = global.cookies;
-      const info = await fetch(constants.graphql, {
-        method: 'POST',
-        headers,
-        body: JSON.stringify({
-          query: `
-            query Notification($after: String!, $count: Int!) {
-              notifications(after: $after, count: $count) {
-                items {
-                  ... on Notification {
-                    id
-                    url
-                    text
-                    seen
-                    context
-                    creator {
-                      ${constants.userAttributes}
+    async allNotification(after, count) {
+      if (!global.cookies) {
+        throw new Error('Not logged in.');
+      } else {
+        headers.Cookie = global.cookies;
+        const info = await fetch(constants.graphql, {
+          method: 'POST',
+          headers,
+          body: JSON.stringify({
+            query: `
+              query Notification($after: String!, $count: Int!) {
+                notifications(after: $after, count: $count) {
+                  items {
+                    ... on Notification {
+                      id
+                      url
+                      text
+                      seen
+                      context
+                      creator {
+                        ${constants.userAttributes}
+                      }
+                      timeCreated
+                      timeUpdated
                     }
-                    timeCreated
-                    timeUpdated
+                  }
+                  pageInfo {
+                    nextCursor
                   }
                 }
-                pageInfo {
-                  nextCursor
-                }
-              }
-            }`,
-          variables: JSON.stringify({
-            after,
-            count,
+              }`,
+            variables: JSON.stringify({
+              after,
+              count,
+            }),
           }),
-        }),
-      })
-        .then((res) => res.json());
+        })
+          .then((res) => res.json());
 
-      if (!info.data.notifications) {
-        throw new Error('Cannot fetch notifications.');
-      } else {
-        return info.data.notifications;
+        if (!info.data.notifications) {
+          throw new Error('Cannot fetch notifications.');
+        } else {
+          return info.data.notifications;
+        }
       }
     }
-  }
+  };
+} else {
+  exportable = function noExperimentalFeatures() {
+    console.log('Experimental Features are not enabled. To use learn more about experimental features please visit the documentation.');
+  };
 }
+
+export default exportable;
